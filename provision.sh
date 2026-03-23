38d10a989e710c72c61465c2c6677f2b56c086bb
Nathaniel Abalos <nathaniel@dedesignworks.com>
2026-03-23 15:37:05 -0500

    Enhance provision script to enable WiFi and configure access point

--- provision.sh
+++ provision.sh
@@ -8,12 +8,30 @@ fi
 
 apt update
 
+echo "Enabling SPI via raspi-config"
 raspi-config nonint do_spi 0
+
+echo "Enabling I2C via raspi-config"
 raspi-config nonint do_i2c 0
+
+echo "Enabling Serial Port via raspi-config"
 raspi-config nonint do_serial_hw 0
+
+echo "Disabling Serial Console via raspi-config"
 raspi-config nonint do_serial_cons 1
 
+echo "Enabling WiFi"
+rfkill unblock wifi
+ifconfig wlan0 up
+
+nmcli radio wifi on
+
+echo "Enable WiFi Access Point"
 ser_num=$(cat /sys/firmware/devicetree/base/serial-number | cut -c9-16)
 nmcli device wifi hotspot ssid deteq_ap-${ser_num} password ${ser_num} ifname wlan0
 
+# Set the hotspot to autoconnect with high priority
+nmcli connection modify Hotspot connection.autoconnect yes connection.autoconnect-priority 100
+
+echo "Rebooting to apply changes"
 reboot

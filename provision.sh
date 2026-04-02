bbfaedae3338ecd87c638ca2f5ebac3b888ab51a
Nathaniel Abalos <nathaniel@dedesignworks.com>
2026-04-02 14:05:18 -0500

    Add USB mount configuration and ownership setup in provisioning script

--- provision.sh
+++ provision.sh
@@ -35,6 +35,20 @@ nmcli device wifi hotspot ssid deteq_ap-${ser_num} password ${ser_num} ifname wl
 # Set the hotspot to autoconnect with high priority
 nmcli connection modify Hotspot connection.autoconnect yes connection.autoconnect-priority 100
 
+# create /mnt/usb directory if it doesn't exist
+if [ ! -d "/mnt/usb" ]; then
+  mkdir -p /mnt/usb
+fi
+
+# change the owner of the /mnt/usb directory to pi
+chown pi:pi /mnt/usb
+
+# add a line to /etc/fstab to mount the USB drive at /mnt/usb
+# we will use /dev/sda1 as the device, but this may need to be changed depending on the system
+if ! grep -q "/dev/sda1" /etc/fstab; then
+  echo "/dev/sda1 /mnt/usb auto defaults,uid=pi,gid=pi,nofail 0 0" >> /etc/fstab
+fi
+
 # clean up the provisioning files
 # delete the parent folder of this script
 script_dir=$(dirname "$(realpath "$0")")
